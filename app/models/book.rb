4ca043085acdd68aec955cc365d8c0c16c461f36
kyamax <kisho.0125@gmail.com>
2022-11-01 22:21:13 +0900

    本の投稿機能の実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,10 @@ class Book < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :genre
-  
+  has_one_attached :image
+
+  validates :image, presence: true
+  validates :book_name, presence: true
+  validates :author, presence: true
+  validates :genre_id, numericality: { other_than: 1, message: "can't be blank" }
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -6,11 +6,11 @@ class Genre < ActiveHash::Base
     { id: 4, name: '2 歴史' },
     { id: 5, name: '3 社会科学' },
     { id: 6, name: '4 自然科学' },
-    { id: 7, name: '5 技術・工学・工業' }
-    { id: 8, name: '6 産業' }
-    { id: 9, name: '7 芸術・美術' }
-    { id: 10, name: '8 言語' }
-    { id: 11, name: '9 文学' }
+    { id: 7, name: '5 技術・工学・工業' },
+    { id: 8, name: '6 産業' },
+    { id: 9, name: '7 芸術・美術' },
+    { id: 10, name: '8 言語' },
+    { id: 11, name: '9 文学' },
     { id: 12, name: 'F 絵本' }
   ]
 
