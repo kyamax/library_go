a4d5f63a8fa3cdd1070af3f70526b501b4590a19
kyamax <kisho.0125@gmail.com>
2022-11-05 20:41:44 +0900

    google books apiの仮導入

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,6 @@ class CommentsController < ApplicationController
 
 private
   def comment_params
-    params.require(:comment).permit(:content, :user_id, :prototype_id).merge(user_id: current_user.id, book_id: params[:book_id])
+    params.require(:comment).permit(:content).merge(user_id: current_user.id, book_id: params[:book_id])
   end
 end
