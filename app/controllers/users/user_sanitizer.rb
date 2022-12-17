b20ebcb2f6506d03583ed85ac14eb21385bb9b4c
kyamax <kisho.0125@gmail.com>
2022-12-17 18:33:55 +0900

    sanitizerのコメントアウト削除

--- app/controllers/users/user_sanitizer.rb
+++ app/controllers/users/user_sanitizer.rb
@@ -1,17 +1,17 @@
-# class User::ParameterSanitizer < Devise::ParameterSanitizer
-#   def initialize(*)
-#     super
-#     permit(:sign_up, keys: [:school, :grade_id, :class_num_id, :number_id, :name])
-#     permit(:sign_in, keys: [:grade_id, :class_num_id, :number_id])
-#   end
-# end
+class User::ParameterSanitizer < Devise::ParameterSanitizer
+  def initialize(*)
+    super
+    permit(:sign_up, keys: [:school, :grade_id, :class_num_id, :number_id, :name])
+    permit(:sign_in, keys: [:grade_id, :class_num_id, :number_id])
+  end
+end
 
-# class UserParameterSanitizer < Devise::ParameterSanitizer
-#   def sign_up
-#     default_params.permit(:school, :grade_id, :class_num_id, :number_id, :name)
-#   end
+class UserParameterSanitizer < Devise::ParameterSanitizer
+  def sign_up
+    default_params.permit(:school, :grade_id, :class_num_id, :number_id, :name)
+  end
 
-#   def sign_in
-#     default_params.permit(:grade_id, :class_num_id, :number_id)
-#   end
-# end
+  def sign_in
+    default_params.permit(:grade_id, :class_num_id, :number_id)
+  end
+end
