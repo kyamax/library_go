385ee02a57c6dd3a169971cccd302f8a92a14e70
kyamax <kisho.0125@gmail.com>
2022-12-17 19:06:19 +0900

    コメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,20 @@
 class ApplicationController < ActionController::Base
-  before_action :devise_parameter_sanitizer, if: :devise_controller?
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :devise_parameter_sanitizer, if: :devise_controller?
+  # before_action :configure_permitted_parameters, if: :devise_controller?
 
   
-  private
+  # private
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:school, :grade_id, :class_num_id, :number_id, :name, :employee_id])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:grade_id, :class_num_id, :number_id, :employee_id])
-  end
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:school, :grade_id, :class_num_id, :number_id, :name, :employee_id])
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:grade_id, :class_num_id, :number_id, :employee_id])
+  # end
 
-  def devise_parameter_sanitizer
-    if resource_class == User
-      User::ParameterSanitizer.new(User, :user, params)
-    else
-      super # Use the default one
-    end
-  end
+  # def devise_parameter_sanitizer
+  #   if resource_class == User
+  #     User::ParameterSanitizer.new(User, :user, params)
+  #   else
+  #     super # Use the default one
+  #   end
+  # end
 end
