330c67cc2ac9dd1be9541b7d770403d3dfdb0d3f
kyamax <kisho.0125@gmail.com>
2022-12-27 18:47:09 +0900

    学校登録機能4（yarn設定）

--- app/javascript/packs/hello_vue.js
+++ app/javascript/packs/hello_vue.js
@@ -23,7 +23,7 @@ import axios from 'axios';
 
 axios.defaults.headers.common = {
   'X-Requested-With': 'XMLHttpRequest',
-  'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+  'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
 };
 
 new Vue({
