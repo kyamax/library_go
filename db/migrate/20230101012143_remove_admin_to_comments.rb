06b55d427675c20bf54a482af7c16df54f9a3100
kyamax <kisho.0125@gmail.com>
2023-01-01 13:46:02 +0900

    Delete 20230101012143_remove_admin_to_comments.rb

--- db/migrate/20230101012143_remove_admin_to_comments.rb
+++
@@ -1,6 +0,0 @@
-class RemoveAdminToComments < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :comments, :admin_id, :reference
-    remove_column :comments, :admin_id_id, :reference
-  end
-end
