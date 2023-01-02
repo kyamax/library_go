7af230abebf71147ac6c427a0a4bbc971e56853a
kyamax <kisho.0125@gmail.com>
2023-01-02 12:54:25 +0900

    学校登録機能の修正（都道府県登録のコメントアウト）

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,11 +1,12 @@
 class SchoolsController < ApplicationController
   def new
-    gon.prefectures = Prefecture.all.to_json only: %i[id name]
+    # gon.prefectures = Prefecture.all.to_json only: %i[id name]
     @school = School.new
   end
 
   def create
     @school = School.new(school_params)
+    binding.pry
     if @school.valid?
       @school.save
       redirect_to schools_path
@@ -17,7 +18,8 @@ class SchoolsController < ApplicationController
   private
 
   def school_params
-    params.require(:school).permit(:name, :prefecture_id, :city_id)
+    params.require(:school).permit(:name)
+    # params.require(:school).permit(:name, :prefecture_id, :city_id)
   end
 
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -2,8 +2,8 @@ class School < ApplicationRecord
   has_many :prefectures
   has_many :cities
 
-  belongs_to :admin
-  belongs_to :user
+  belongs_to :admin, optional: true
+  belongs_to :user, optional: true
 
   validates :name, presence: true
   
